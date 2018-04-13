487d9bf321be8094441e966661c1dfaf542e7749
Raissa Arruda <Raissa Arruda@DESKTOP-C3A2R2V>
2018-04-13 19:28:13 -0300

    first commit

---
+++ projeto/accounts/urls.py
@@ -0,0 +1,12 @@
+# accounts/urls.py
+from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
+from . import views
+app_name='accounts'
+
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+]
