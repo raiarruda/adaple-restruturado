da8c18bb66f4238f1ed5fb3ff76414a709bee910
raissa <raiarruda.raa@gmauil.com>
2018-11-27 17:36:45 -0300

    forum

--- projeto/boards/forms.py
+++ projeto/boards/forms.py
@@ -6,11 +6,13 @@ from .models import Topic
 class NewTopicForm(forms.ModelForm):
     message = forms.CharField(
         widget=forms.Textarea(
-            attrs={'rows': 5, 'placeholder': 'What is in your mind?'}
+            attrs={'rows': 5, 'placeholder': 'O que você pensa?',}
         ),
+        label="Mensagem",
         max_length=4000,
-        help_text='The max length of the text is 4000.'
+        help_text='Tamanho máximo 4000 caracteres.'
     )
+    subject=forms.CharField(max_length=150, label="Assunto")
 
     class Meta:
         model = Topic
